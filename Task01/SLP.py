e9e7bf617b49155244f77a7d078f2c73f5599f4c
UC <yousefgus111@gmail.com>
2024-10-29 11:29:11 +0300

    confusion matrix

--- Task01/SLP.py
+++ Task01/SLP.py
@@ -29,16 +29,35 @@ class SLP():
                     self.weights += self.learning_rate * error * X.iloc[i].values
                     self.bias += self.learning_rate * error
 
-    def predict(self, X, Y):
-        TP, FP = 0, 0
-        FN, TN = 0, 0
+    def predict(self, X, column_name):
+
+        Y_pred = []
         for i in range(X.shape[0]):
             # Calculate the weighted sum
             linear_output = np.dot(X.iloc[i].values, self.weights) + self.bias
 
             # Apply signum function
             y_pred = 1 if linear_output >= 0 else -1
+
+            Y_pred.append([y_pred])
+
+        return pd.DataFrame(Y_pred, columns=[column_name])
+
+    def accuracy_score(self, Y, Y_predict):
+        # Calculate the number of correct predictions
+        correct_predictions = (Y.values.flatten() == Y_predict.values.flatten()).sum()
+        
+        # Calculate accuracy
+        accuracy = correct_predictions / len(Y)
+
+        return accuracy
+
+    def confusion_matrix(self, Y, Y_pred):
+        TP, FP = 0, 0
+        FN, TN = 0, 0
+        for i in range(Y.shape[0]):
             y = Y.iloc[i, 0]
+            y_pred = Y_pred.iloc[i, 0]
 
             if y_pred == 1:
                 if y == 1: TP+=1 
@@ -47,6 +66,10 @@ class SLP():
                 if y == 1: FN+=1 
                 else: TN+=1
 
-        accuracy = (TP + TN) / X.shape[0]
+        matrix = df = pd.DataFrame({
+            'Predicted Positive': [TP, FP],
+            'Predicted Negative': [FN, TN]
+        }, index=['Actual Positive', 'Actual Negative'])
+        
+        return matrix
 
-        return accuracy
